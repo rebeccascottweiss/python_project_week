eaf8e1843d2f00c912d2c8b9f45eb0775a1d14b5
Nathaniel Burge <NathanielBurge@users.noreply.github.com>
2020-10-19 15:34:46 -0700

    latest changes to model

--- patron_app/models.py
+++ patron_app/models.py
@@ -1,11 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Payment(models.Model):
+    payment_type = models.CharField(max_length=255) #CC or paypal
+    vendor_id = models.CharField(max_length=255) #identification with 3rd party processor
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Identification(models.Model):
+    ident_type = models.CharField(max_length=255) # DL or other state ID
+    ident_num  = models.CharField(max_length=255) # number on state issued ID
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    dob = models.Date()
+    exp = models.Date()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 class User(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    age = intField()
+    age = models.IntegerField()
     email_address = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
+    identification = models.ForeignKey(Identification, related_name="users", on_delete=models.CASCADE)
+    payments = models.ForeignKey(Payment, related_name="users", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
