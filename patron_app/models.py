5a7ba6f60f991e87c3fc9485b1f5fa56e0048fee
Nathaniel Burge <NathanielBurge@users.noreply.github.com>
2020-10-19 14:35:09 -0700

    updated models.py

--- patron_app/models.py
+++ patron_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    age = intField()
+    email_address = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
