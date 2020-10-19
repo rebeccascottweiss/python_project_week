d6f243152b48c7ae25a4013844ada396c99dcf89
Nathalie Souza <nsouza91@yahoo.com>
2020-10-19 14:24:34 -0700

    created bar and drinks model
    created bar and drinks model

--- bar_app/models.py
+++ bar_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+class Bar(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    #bartenders - related name for bartender id
+    #tabs - related name for tabs
+    #drinks - related name for drinks 
+
+class Drink(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField
+    cost = models.IntegerField()
+    bar = models.ForeignKey(Bar, related_name='drinks', on_delete= models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
