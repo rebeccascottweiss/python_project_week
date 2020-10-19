bfe02b8442e2bd6819f6ba3186d6a8a6c98e40d5
Rebecca Weiss <rebeccascottweiss@gmail.com>
2020-10-19 14:20:22 -0700

    Update models.py
    Changed Employee and Tab model

--- bar_app/models.py
+++ bar_app/models.py
@@ -5,6 +5,7 @@ class Employee(models.Model):
     employee_id = models.IntegerField()
     is_manager = models.BooleanField(null=True)
     password = models.CharField(max_length=255)
+    bar = models.ForeignKey(Bar, related_name='bartenders', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     #Took out the tips field - Save tips in Session? 
@@ -14,8 +15,8 @@ class Tab(models.Model):
     patron = models.ForeignKey(User, related_name='tabs', on_delete=models.CASCADE)
     drinks = models.ManyToManyField(Drink, related_name='patron_tabs')
     payment_reference = models.CharField(max_length=500, null=True)
+    total=models.IntegerField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     #Took out "Tab Num" - can be interchangable with ID
-    #Took out "Total" - But could add it back in. Do we want the 
-    #app to store tab totals for record keeping? 
+    
