ee92d6fb39a6bb97026329b3fda8e91f9061e925
Rebecca Weiss <rebeccascottweiss@gmail.com>
2020-10-19 13:56:12 -0700

    Basic Setup and Employee/Tab models
    Added bar_app to the list of apps in settings.py and the 'bar/' path to the project urls.
    Built Employee and Tab model with a couple of changes from the ERD. Added notes in the code about it... I can add things in if needed.

--- bar_app/models.py
+++ bar_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=255)     
+    employee_id = models.IntegerField()
+    is_manager = models.BooleanField(null=True)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    #Took out the tips field - Save tips in Session? 
+    #because they change and would be reset with each shift? 
+
+class Tab(models.Model):
+    patron = models.ForeignKey(User, related_name='tabs', on_delete=models.CASCADE)
+    drinks = models.ManyToManyField(Drink, related_name='patron_tabs')
+    payment_reference = models.CharField(max_length=500, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    #Took out "Tab Num" - can be interchangable with ID
+    #Took out "Total" - But could add it back in. Do we want the 
+    #app to store tab totals for record keeping? 
--- pay_my_tab/urls.py
+++ pay_my_tab/urls.py
@@ -18,4 +18,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('bar/', include('bar_app.urls')),
 ]
