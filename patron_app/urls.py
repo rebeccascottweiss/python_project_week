15347aca4b032b9055ac5ac7d76aa31ed20c2205
HBDanks <danksharold@gmail.com>
2020-10-20 16:39:23 -0700

    pushing today's work
    Added account, receipt, patron_tip templates.
    - Added a navbar to patron_home as well as a tab history to track display previous tabs.
    - Updated account links to go to /patron/account.
    - Updated a few instances of 'user' in model to 'patron'
    - Updated paymen_info navbar
    - Update Urls to reflect current urls
    - Added update_info, pay_tab, tab_receipt, account, tip_select and return_home functions to views
    - updated start_tab function to create a new tab if a user already had payment info
    - other small changes to views

--- patron_app/urls.py
+++ patron_app/urls.py
@@ -9,5 +9,10 @@ urlpatterns = [
     path('add_payment', views.add_payment),
     path('patron_tab', views.patron_tab),
     path('pay_tab', views.pay_tab),
+    path('tab_receipt', views.tab_receipt),
+    path('patron/account', views.account),
+    path('patron/update_info', views.update_info),
+    path('tip_select', views.tip_select),
+    path('return_home', views.return_home),
     path('logout', views.logout),
 ]
