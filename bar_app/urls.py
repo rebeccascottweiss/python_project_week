7f8eede99f673cf523aea11b55be7c10bdf86771
Nathalie Souza <nsouza91@yahoo.com>
2020-10-19 20:19:40 -0700

    Final nightly changes
    final changes to some manager views and htmls

--- bar_app/urls.py
+++ bar_app/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('register', views.register),
+    path('managerdash/addemployee/register', views.register),
     path('login', views.login),
     path('dashboard',views.dashboard),
+    path('dashboard/managerdash', views.managerdash),
     path('<tab_id>/close_out', views.close_out),
     path('addemployee', views.addemployee),
-    path('addemployee/new', views.newemployee),
     path('adddrink', views.adddrink),
     path('adddrink/new', views.newdrink),
     path('drinks', views.drinks)
