cbe9bd38f9c34eb7e74587e948787ae1a71a28d5
Rebecca Weiss <rebeccascottweiss@gmail.com>
2020-10-20 17:35:27 -0700

    Lots of debugging
    Fixed the switch employee functionality on bar dashboard. Created "pick up tab" functionality (we can change the name) and an add drink functionality on tabs.html.

--- bar_app/urls.py
+++ bar_app/urls.py
@@ -7,8 +7,7 @@ urlpatterns = [
     path('login', views.login),
     path('cashout', views.cashout),
     path('dashboard',views.dashboard),
-    path('drinks/delete/<number>', views.deletedrink)
-    path('dashboard/managerdash', views.managerdash),
+    path('drinks/delete/<number>', views.deletedrink),
     path('addemployee', views.addemployee),
     path('adddrink', views.adddrink),
     path('adddrink/new', views.newdrink),
@@ -18,4 +17,6 @@ urlpatterns = [
     path('delete_drink/<tab_id>/<drink_id>', views.delete_drink),
     path('close_out/<tab_id>', views.close_out),
     path('edit/<tab_id>', views.edit_tab),
+    path('pick_up/<tab_id>', views.pick_up),
+    path('drop/<tab_id>', views.drop),
 ]
--- patron_app/urls.py
+++ patron_app/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('start_tab', views.start_tab),
     path('add_payment', views.add_payment),
     path('patron_tab', views.patron_tab),
-    path('pay_tab', views.pay_tab),
+    # path('pay_tab', views.pay_tab),
     path('logout', views.logout),
 ]
