c99d059debc28d9c100f443c2dee8d5df04e7c61
HBDanks <danksharold@gmail.com>
2020-11-18 20:49:24 -0800

    admin playground
    uncommented the admin path to mess around with it

--- patron_app/admin.py
+++ patron_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Payment, Patron, Identification
+#from ../bar_app/models import EmployeeManager, Bar, Drink, Employee, Tab, LineItem
 
 # Register your models here.
+
+admin.site.site_header = "Trey's playground"
+
+admin.site.register(Patron)
+admin.site.register(Payment)
+admin.site.register(Identification)
+#admin.site.register(EmployeeManager)
+# admin.site.register(Bar)
+# admin.site.register(Drink)
+# admin.site.register(Employee)
+# admin.site.register(Tab)
+# admin.site.register(LineItem)
