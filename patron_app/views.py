119158f6ebf6292e024631208f02630dd579acf7
Rebecca Weiss <rebeccascottweiss@gmail.com>
2020-12-30 17:23:41 -0800

    hidden keys

--- patron_app/views.py
+++ patron_app/views.py
@@ -4,7 +4,8 @@ from .models import Patron
 from bar_app.models import Tab
 import bcrypt
 import stripe
-stripe.api_key = "sk_test_51HfHFiJs7YlP147G1rg96ffuvMG5hXYqZHU0hzuYWL2jbL3IjYmbGvxOoKhVoUElXz8CD7GvhkvV6pQn8iAs0H7z00NMXmpSh3"
+import os
+stripe.api_key = os.environ['stripe.api_key']
 
 
 
